e49f0625a57254ba6c42caef0d8455da3fbbd6b5
Ben Sutton <suttonbm@gmail.com>
2024-12-01 08:34:42 -0500

    Initial commit

---
+++ src/main/java/net/suttonbm/aoc2024/AoC2024Application.java
@@ -0,0 +1,13 @@
+package net.suttonbm.aoc2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoC2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoC2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/net/suttonbm/aoc2024/AoC2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.suttonbm.aoc2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoC2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
